8b1c3aba8d677590a05a331d18cb2e6f4e931d8d
Trường An <54493797+truongankg1996@users.noreply.github.com>
2019-11-05 15:20:57 +0700

    Khoi Tạo Dự Án

---
+++ WebBanHang/WebBanHang/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebBanHang
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //route trang Giới Thiệu
+            //url: /gioithieu
+            routes.MapRoute(
+                name: "page.gioi_thieu",
+                url: "gioi-thieu",
+                defaults: new { controller = "Page", action = "GioiThieu"}
+                );
+
+            //Route mặc định của Trang  web
+            //Url: /
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Page", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
